a69b42a3c6f10734177b01c181c9271972f75fa9
Wabisuke0511 <crossclaymorezero@gmail.com>
2026-07-12 09:21:29 +0900

    PWA対応: manifestとService Worker(tokushuzan-v1)を追加

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'tokushuzan-v1';
+
+const PRECACHE = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
